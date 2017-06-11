8321f76d8b8a8c61d6e6b2fe44768f9d4bb5a267
pacdiv <pacdiv@users.noreply.github.com>
2017-06-11 07:24:30 +0200

    Added babeljs into webpack configuration

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   entry: './handler.js',
-  target: 'node'
+  target: 'node',
+  output: {
+    filename: 'handler.js',
+    libraryTarget: 'commonjs2',
+    path: __dirname + '/dist'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  }
 }
